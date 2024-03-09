f20bedec6c0cb5427140076478bb5070e95cab6a
Nikolya <Nikolya2104@mail.ru>
2024-03-09 11:49:49 +0300

    need help, but good

--- mysite/my_codes/views.py
+++ mysite/my_codes/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from django.utils.datastructures import MultiValueDictKeyError
-
+from passlib.hash import pbkdf2_sha256
 from .forms import SearchUserForm
 from .models import Account, NIKNEM, Avatar
 import re
@@ -24,7 +24,7 @@ def register_page(request):
             account_exists = Account.objects.filter(email=email).exists()
             if not account_exists:
                 username = name.lower() + '_' + second_name.lower()
-                user = User.objects.create_user(first_name=name, username=username, last_name=second_name, email=email,password=hashed_password)
+                user = User.objects.create_user(first_name=name, username=username, last_name=second_name, email=email,password=password)
 
                 table_item = Account(name=name, second_name=second_name, email=email, password=hashed_password)
                 table_item.save()
@@ -70,15 +70,19 @@ def login_page (request) :
         users = User.objects. filter (email=email)
         if users.exists():
             for user in users:
-                if check_password (make_password(password), user.password):
+                if check_password(password, user.password):
                     print ("good")
-                    request. session[ 'username'] = user.username
-                    request. session[ 'email'] = user.email
+
+                    request.session['username'] = user.first_name
+                    request.session['user_seconds'] = user.last_name
+                    request.session['email'] = user.email
+
                     context[ "good"] = "Вы успешно зашли в аккаунт"
                     return render (request,'login.html', context)
                 else:
+                    print(make_password(password),password)
                     print ("bad" )
-                context[ "error"] = "Попробуйте ввести деугой дароль"
+                    context[ "error"] = "Попробуйте ввести другой пароль"
         else:
             context ["error"] = "Такого пользователя нет"
     return render(request,"login.html", context)
@@ -92,18 +96,16 @@ def open_page(request):
 
 
 def account_page(request):
-    name = request.session.get('username')
-    second_name = request.session.get('user_seconds')
-    email = request.session.get('email')
+    context = {'name': User.first_name, 'second_name': User.last_name, 'email': User.email}
 
     try:
-        account = Account.objects.filter(name=name, second_name=second_name, email=email).first()
-        niknem = NIKNEM.objects.filter(account=account).first()
+        user = User.objects.filter(username=context['name'], last_name=context['second_name'], email=context['email']).first()
 
-        if not account:
-            raise Account.DoesNotExist
+        if not user:
+            raise User.DoesNotExist
 
-        avatar = Avatar.objects.filter(account=account).first()
+        niknem = NIKNEM.objects.filter(account=user).first()
+        avatar = Avatar.objects.filter(account=user).first()
 
         if request.method == 'POST' and 'avatarInput' in request.FILES:
             image = request.FILES['avatarInput']
@@ -112,19 +114,17 @@ def account_page(request):
                 avatar.image = image
                 avatar.save()
             else:
-                new_avatar = Avatar(image=image, account=account)
+                new_avatar = Avatar(image=image, account=user)
                 new_avatar.save()
             return redirect('account_page')
 
-        if request.method == 'GET':
-            context = {'account': account, 'avatar': avatar, 'niknem': niknem}
-            return render(request, 'account_page.html', context)
-        else:
-            return HttpResponseBadRequest()
+        context = {'account': user, 'avatar': avatar, 'niknem': niknem}
+        return render(request, 'account_page.html', context)
 
-    except Account.DoesNotExist:
+    except User.DoesNotExist:
         context = {'error': 'Такого пользователя нет'}
         return render(request, 'account_page.html', context)
+
 
 
 def remember_password(request):
@@ -135,14 +135,14 @@ def remember_password(request):
         email = request.POST.get("email")
         password = request.POST.get("password")
         try:
-            account = Account.objects.filter(name=name, second_name=second_name, email=email).first()
-            if account:
-                account.password = make_password(password)
-                account.save()
+            user = User.objects.filter(first_name=name, last_name=second_name, email=email).first()
+            if user:
+                user.set_password(password)
+                user.save()
                 context['good'] = "Пароль успешно изменен"
             else:
                 context['error'] = "Пароль не сохранен"
-        except Account.DoesNotExist:
+        except User.DoesNotExist:
             context['error'] = 'Произошла ошибка при попытке изменения пароля'
 
     return render(request, "remember_password.html", context)
@@ -152,8 +152,7 @@ def achievements(request):
     return render(request, "Achievements.html")
 
 
-from django.shortcuts import render
-from .models import NIKNEM
+
 
 def find_users_page(request):
     context = {}
@@ -180,7 +179,10 @@ def find_users_page(request):
 
     return render(request, "find_users.html", context)
 def home_page(request):
-    return render(request,'homePage.html')
+    context={}
+
+    context['username']=f'Welcome'
+    return render(request,'homePage.html',context)
 
 def turnir_page(request):
     return render(request,'turnir_page.html')
